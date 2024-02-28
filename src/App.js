30e93aa1722a48081def42a2410203898650d50d
Hong-Eungyo <vjvlaos75@naver.com>
2024-02-28 14:21:47 +0900

    Add hompage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import Menu_all from './component/Menu_all';
+import Home from './component/Home';
+import About from './component/About';
+import Shop from './component/Shop';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrap'>
+      <BrowserRouter  basename={process.env.PUBLIC_URL}>
+        <Menu_all></Menu_all>
+
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/home" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/shop" element={ <Shop /> } />
+        </Routes>
+
+    </BrowserRouter>
     </div>
   );
 }
